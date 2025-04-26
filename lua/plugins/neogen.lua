21a3a738e415ae21ef8624b0090613422a0f62e5
Ernesto-B <ernesto.barreto@ucalgary.ca>
2025-04-25 19:53:22 -0600

    feat: added neogen automatic docstring generation

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,52 @@
+return {
+    "danymat/neogen",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter", -- for AST-powered introspection
+        "L3MON4D3/LuaSnip",
+    },
+    -- load on demand when you hit your keybinding
+    cmd = "Neogen",
+    keys = {
+        {
+            "<leader>cn",
+            function()
+                require("neogen").generate()
+            end,
+            desc = "📝 Neogen: generate documentation",
+        },
+    },
+    config = function()
+        require("neogen").setup({
+            -- pick your snippet engine: "luasnip", "vsnip", "snippy", etc.
+            snippet_engine = "luasnip",
+
+            -- automatically insert your cursor inside the docblock after generating
+            input_after_comment = true,
+
+            -- per-language annotation conventions
+            languages = {
+                lua = {
+                    template = {
+                        annotation_convention = "ldoc",
+                    },
+                },
+                python = {
+                    template = {
+                        annotation_convention = "google_docstrings",
+                    },
+                },
+                javascript = {
+                    template = {
+                        annotation_convention = "jsdoc",
+                    },
+                },
+                typescript = {
+                    template = {
+                        annotation_convention = "tsdoc",
+                    },
+                },
+                -- add more here if you like…
+            },
+        })
+    end,
+}
